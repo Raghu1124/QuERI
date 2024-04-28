66815b6ed1f01f890c191ec77e65639258363d0f
Raghunandan Bansal <raghubansal1102@gmail.com>
2024-04-28 13:46:05 +0530

    splash screen updated

--- app/src/main/java/com/example/newgem/SplashScreen.kt
+++ app/src/main/java/com/example/newgem/SplashScreen.kt
@@ -1,10 +1,12 @@
 package com.example.newgem
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.LinearLayout
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +15,22 @@ class SplashScreen : AppCompatActivity() {
 
         window.statusBarColor = getColor(R.color.back)
         window.navigationBarColor = getColor(R.color.back)
+        val textview = findViewById<TextView>(R.id.name)
+        val title = "Questions Evaluated and Responded by Intelligence"
+        val spannableTitle = SpannableString(title)
+        spannableTitle.setSpan(
+            ForegroundColorSpan(getColor(R.color.blue)), 0, 9, 0
+        )
+        spannableTitle.setSpan(
+            ForegroundColorSpan(getColor(R.color.green)), 10, title.length, 0
+        )
+        textview.text = spannableTitle
 
-        val splashScreen= findViewById<LinearLayout>(R.id.mainlogo)
-        splashScreen.alpha= 0f
+
+        val splashScreen = findViewById<RelativeLayout>(R.id.splashscreen)
+        splashScreen.alpha = 0f
         splashScreen.animate().setDuration(4000).alpha(1f).withEndAction {
-            val intent= Intent(this, MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
